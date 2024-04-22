d80175b4d4b8276698b2b5fe42096445e74212f1
Tommaso <tommaso.brini@studio.unibo.it>
2024-04-22 11:26:34 +0200

    Solved notTwoConsecutive ex1

--- src/main/scala/ex1/Parser.scala
+++ src/main/scala/ex1/Parser.scala
@@ -1,9 +1,10 @@
 package ex1
 
-/** Consider the Parser example shown in previous lesson. Analogously to NonEmpty, create a mixin NotTwoConsecutive,
-  * which adds the idea that one cannot parse two consecutive elements which are equal. Use it (as a mixin) to build
-  * class NotTwoConsecutiveParser, used in the testing code at the end. Note we also test that the two mixins can work
-  * together!!
+/** Consider the Parser example shown in previous lesson. 
+ * Analogously to NonEmpty, create a mixin NotTwoConsecutive, which adds 
+ * the idea that one cannot parse two consecutive elements which are equal. 
+ * Use it (as a mixin) to build class NotTwoConsecutiveParser, used in the testing code 
+ * at the end. Note we also test that the two mixins can work together!!
   */
 
 abstract class Parser[T]:
@@ -12,7 +13,7 @@ abstract class Parser[T]:
   def parseAll(seq: Seq[T]): Boolean = (seq forall parse) & end // note &, not &&
 
 object Parsers:
-  val todo = ??? // put the extensions here..
+  val todo = ??? // put the extensions here.. 
 class BasicParser(chars: Set[Char]) extends Parser[Char]:
   override def parse(t: Char): Boolean = chars.contains(t)
   override def end: Boolean = true
@@ -20,17 +21,22 @@ class BasicParser(chars: Set[Char]) extends Parser[Char]:
 trait NonEmpty[T] extends Parser[T]:
   private[this] var empty = true
   abstract override def parse(t: T): Boolean =
-    empty = false;
+    empty = false
     super.parse(t) // who is super??
   abstract override def end: Boolean = !empty && super.end
 
 class NonEmptyParser(chars: Set[Char]) extends BasicParser(chars) with NonEmpty[Char]
 
 trait NotTwoConsecutive[T] extends Parser[T]:
-  val todo = ???
-// ???
+  var prec: Option[T] = Option.empty 
+  abstract override def parse(t: T): Boolean = (prec.isDefined) match
+    case true if (prec.get == t) => false
+    case _ => 
+      prec = Option.apply(t)
+      super.parse(t)
 
-class NotTwoConsecutiveParser(chars: Set[Char]) extends BasicParser(chars) // with ????
+
+class NotTwoConsecutiveParser(chars: Set[Char]) extends BasicParser(chars) with NotTwoConsecutive[Char]
 
 @main def checkParsers(): Unit =
   def parser = new BasicParser(Set('a', 'b', 'c'))
